37492c364fa6b3f8b24db225f88e7c78e9dfc790
blomq <blomqvistkevin@hotmail.com>
2025-08-13 00:00:12 +0200

    Refactor `InputManager` by moving `forEachBinding` implementation to a new `.tpp` file for improved header clarity and maintainability.

--- include/psyengine/input_manager.hpp
+++ include/psyengine/input_manager.hpp
@@ -209,19 +209,8 @@ namespace psyengine
         static std::string getGamepadAxisName(SDL_GamepadAxis axis);
     };
 
-    template <typename Func>
-    bool InputManager::forEachBinding(const std::string& actionName, Func&& func) const
-    {
-        if (const auto it = actions_.find(actionName); it != actions_.end())
-        {
-            for (const auto& binding : it->second.bindings)
-            {
-                if (func(binding, *this))
-                    return true; // any binding is enough to return true
-            }
-        }
-        return false;
-    }
+#include "input_manager.tpp"
+
 }
 
 #endif //PSYENGINE_INPUT_MANAGER_HPP
---
+++ include/psyengine/input_manager.tpp
@@ -0,0 +1,18 @@
+﻿#ifndef INPUT_MANAGER_TPP
+#define INPUT_MANAGER_TPP
+
+template <typename Func>
+bool InputManager::forEachBinding(const std::string& actionName, Func&& func) const
+{
+    if (const auto it = actions_.find(actionName); it != actions_.end())
+    {
+        for (const auto& binding : it->second.bindings)
+        {
+            if (func(binding, *this))
+                return true; // any binding is enough to return true
+        }
+    }
+    return false;
+}
+
+#endif // INPUT_MANAGER_TPP
