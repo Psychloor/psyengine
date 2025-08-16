12f14e612b9fe992375374eace72dd439bb8f658
blomq <blomqvistkevin@hotmail.com>
2025-08-16 21:42:53 +0200

    Add `assert.hpp` to `psyengine/debug` for assertion handling
    - Introduced `Assert` and `DebugAssert` functions leveraging SDL assertions.
    - Added `PSY_ASSERT` and `PSY_DEBUG_ASSERT` macros for simplified usage.
    - Ensured conditional behavior for assertions based on `NDEBUG` definition.

---
+++ include/psyengine/debug/assert.hpp
@@ -0,0 +1,44 @@
+﻿//
+// Created by blomq on 2025-08-16.
+//
+
+#ifndef PSYENGINE_ASSERT_HPP
+#define PSYENGINE_ASSERT_HPP
+
+#include <source_location>
+#include <string_view>
+
+#include <SDL3/SDL_assert.h>
+
+#include "psyengine_export.h"
+
+namespace psyengine::debug
+{
+
+    PSYENGINE_EXPORT inline void Assert(const bool condition,
+                                        [[maybe_unused]] const std::string_view message,
+                                        [[maybe_unused]] const std::source_location& location =
+                                            std::source_location::current())
+    {
+        SDL_assert_always(condition); // NOLINT(*) - SDL knows what they're doing for all platforms
+    }
+
+#ifndef NDEBUG
+    PSYENGINE_EXPORT inline void DebugAssert(const bool condition,
+                                             const std::string_view message,
+                                             const std::source_location location = std::source_location::current())
+    {
+        SDL_assert(condition);
+    }
+#else
+    constexpr void DebugAssert(const bool, const std::string_view,
+                                      const std::source_location& = std::source_location::current())
+    {}
+#endif
+
+} // namespace psyengine::debug
+
+#define PSY_ASSERT(condition, message) ::psyengine::debug::Assert((condition), (message))
+#define PSY_DEBUG_ASSERT(condition, message) ::psyengine::debug::DebugAssert((condition), (message))
+
+#endif //PSYENGINE_ASSERT_HPP
